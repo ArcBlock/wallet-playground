138a7c20bea08f40a95d9d891f0ca14ccc6a8b45
wangshijun <wangshijun2010@gmail.com>
2019-11-14 09:35:31 +0800

    chore: support carry login token from url and redirect if detected

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import qs from 'querystring';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Container from '@material-ui/core/Container';
 import styled from 'styled-components';
 import Helmet from 'react-helmet';
 import Footer from '@arcblock/ux/lib/Footer';
+import { useLocation } from 'react-router-dom';
 
 import Header from './header';
 
 import env from '../libs/env';
+import { setToken } from '../libs/auth';
 
 export default function Layout({ title, children, contentOnly }) {
+  const location = useLocation();
+
+  // If a login token exist in url, set that token in storage
+  useEffect(() => {
+    const params = qs.parse(location.search.slice(1));
+    if (params.loginToken) {
+      setToken(params.loginToken);
+
+      delete params.loginToken;
+      const redirectUrl = `${location.pathname}?${qs.stringify(params)}`;
+      window.history.replaceState({}, window.title, redirectUrl);
+    }
+  }, []);
+
   if (contentOnly) {
     return <Container>{children}</Container>;
   }
