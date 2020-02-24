bc8c233061c7de9520f08af2fe36c0a0d8c6292c
wangshijun <wangshijun2010@gmail.com>
2020-02-25 07:45:43 +0800

    chore: remove useless code

--- api/routes/session.js
+++ api/routes/session.js
@@ -1,6 +1,7 @@
 const env = require('../libs/env');
 const { getTokenInfo } = require('../libs/util');
 const { getExchangeRate } = require('../libs/currency');
+
 const isNetlify = process.env.NETLIFY && JSON.parse(process.env.NETLIFY);
 
 module.exports = {
@@ -21,7 +22,6 @@ module.exports = {
     });
 
     app.get('/api/env', (req, res) => {
-      const isNetlify = process.env.NETLIFY && JSON.parse(process.env.NETLIFY);
       res.type('.js');
       res.send(`window.env = {
   localChainExplorer: "${isNetlify ? 'http://54.84.194.134:8210/node/explorer/txs' : ''}",
