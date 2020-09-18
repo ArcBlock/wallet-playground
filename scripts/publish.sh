9ee976cf3e96fe052b390dcc43695749513b35e3
linchen1987 <linchen.1987@foxmail.com>
2020-09-18 18:42:40 +0800

    ci: remove auto deploy

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,5 @@
+set -e
+
 git config --local user.name "wangshijun"
 git config --local user.email "wangshijun2010@gmail.com"
 
@@ -27,14 +29,4 @@ if [ "$changed" != "" ]; then
 
   # trigger cnpm sync
   node scripts/post-publish.js
-
-  # deploy to remote ABT Node
-  if [ "${ALIYUN_ENDPOINT}" != "" ]; then
-    abtnode deploy . --endpoint ${ALIYUN_ENDPOINT} --access-key ${ALIYUN_ACCESS_KEY} --access-secret ${ALIYUN_ACCESS_SECRET} --skip-hooks
-    echo "deploy to ${ALIYUN_ENDPOINT} success"
-  fi
-  if [ "${AWS_ENDPOINT}" != "" ]; then
-    abtnode deploy . --endpoint ${AWS_ENDPOINT} --access-key ${AWS_ACCESS_KEY} --access-secret ${AWS_ACCESS_SECRET} --skip-hooks
-    echo "deploy to ${ALIYUN_ENDPOINT} success"
-  fi
 fi
